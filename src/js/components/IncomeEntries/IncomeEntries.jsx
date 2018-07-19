279520e09cbabffa6281af07b561a72b0f6cf4af
Alex <alexkmartinez77@gmail.com>
2018-07-19 11:29:08 -0700

    Updated ExpenseEntries.jsx, IncomeEntries.jsx, Summary.jsx

--- src/js/components/IncomeEntries/IncomeEntries.jsx
+++ src/js/components/IncomeEntries/IncomeEntries.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
+// We'll need to import all those action creators.
+import {
+  updateIncomeDescription,
+  updateIncomeAmount,
+  addIncome
+} from './incomeActions';
+
 export default class IncomeEntries extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
+    this.handleAmountInput = this.handleAmountInput.bind(this);
+    this.handleAddIncome = this.handleAddIncome.bind(this);
+  }
+
+  handleDescriptionInput(event) {
+    // dispatch was provided by connect()
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateIncomeDescription(value));
+  }
+
+  handleAmountInput(event) {
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateIncomeAmount(value));
+  }
+
+  handleAddIncome() {
+    const { description, amount, dispatch } = this.props;
+    dispatch(addIncome(description, amount));
   }
 
   render() {
+    const { description, amount, lineItems } = this.props;
     return (
       <div className='card border-success mb-3'>
         <div className='card-header text-white bg-success'>Income Entries</div>
@@ -17,6 +47,8 @@ export default class IncomeEntries extends React.Component {
                 type='text'
                 className='form-control'
                 id='income-description'
+                value={ description }
+                onChange={ this.handleDescriptionInput }
               />
             </div>
             <div className='form-group'>
@@ -27,12 +59,15 @@ export default class IncomeEntries extends React.Component {
                   type='text'
                   className='form-control'
                   id='income-amount'
+                  value={ amount }
+                  onChange={ this.handleAmountInput }
                 />
               </div>
             </div>
             <button
               type='button'
               className='btn btn-success col-12 mb-5'
+              onClick={ this.handleAddIncome }
             >+ Add Income
             </button>
             <table className='table table-sm table-hover'>
@@ -43,10 +78,14 @@ export default class IncomeEntries extends React.Component {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Rent</td>
-                  <td>$1,500.00</td>
-                </tr>
+              {
+                  lineItems.map(lineItem => (
+                    <tr>
+                      <td>{ lineItem.description }</td>
+                      <td>${ lineItem.amount.toFixed(2) }</td>
+                    </tr>
+                  ))
+                }
               </tbody>
             </table>
           </form>
